d9e052816f6c430dbfe097d815edc60efc6d77ad
Gianmarco G <giangarof98@gmail.com>
2022-03-25 00:36:18 -0400

    morgan added

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 const app = express();
 const planetsRouter = require('./routes/planets/planets.router');
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+app.use(morgan('combined'))
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html' ))
+})
 
 module.exports = app;
