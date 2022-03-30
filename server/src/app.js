b04feb8d3559c19603a5c1b5a6358a23961a263b
Gianmarco G <giangarof98@gmail.com>
2022-03-29 22:25:31 -0400

    versioning

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
+const api = require('./routes/api')
 const app = express();
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
 
 app.use(cors({
     origin: 'http://localhost:3000'
@@ -14,9 +13,7 @@ app.use(morgan('combined'))
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
-
-app.use('/planets', planetsRouter);
-app.use('/launches',launchesRouter);
+app.use('/v1', api)
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html' ))
 })
--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -18,7 +18,7 @@ describe('Launches API', () => {
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get('/v1/launches')
                 .expect('Content-Type', /json/)
                 .expect(200)
         });
@@ -48,7 +48,7 @@ describe('Launches API', () => {
     
         test('It should respond with 201 created', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -62,7 +62,7 @@ describe('Launches API', () => {
         })
         test('It should catch missing required properties', async() => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -75,7 +75,7 @@ describe('Launches API', () => {
     
         test('It should catch invalid dates', async() => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithInvalidDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
